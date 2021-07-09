fccc16601ba5985dfedb7f3b4b988fe326d27c0c
Vo Trung Ha <ha.vo@infodation.vn>
2021-07-09 09:36:14 +0700

    update app.contants.ts

--- client/src/app/app.contants.ts
+++ client/src/app/app.contants.ts
@@ -1,3 +1,3 @@
-export const SERVER_AUTH_API_URL = 'thitructuyenvth.xyz:3000';
-export const SERVER_QUESTION_API_URL = 'thitructuyenvth.xyz:3000';
+export const SERVER_AUTH_API_URL = 'thitructuyenvth.xyz:3000/';
+export const SERVER_QUESTION_API_URL = 'thitructuyenvth.xyz:3000/';
 export const SERVER_IMAGE = 'thitructuyenvth.xyz:3000/static/image/';
